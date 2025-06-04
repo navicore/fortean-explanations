e1f1b2c573a8ec678f587a395be740769df8ee2c
Ed Sweeney <ed@onextent.com>
2025-06-03 20:02:45 -0700

    clean

--- scripts/push_reasoning_qwen2-7b.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python scripts/push_to_hub.py \
-  --model-path models/fortean_reasoning \
-  --repo-name navicore/fortean-qwen2-7b-instruct \
-  --hf-token $HUGGING_FACE_PUBLISH
-
